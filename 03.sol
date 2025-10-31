472450b17d39f5d206d17bf865becf34fe83319a
VARSHITJAISWAL <161512294+VARSHITJAISWAL@users.noreply.github.com>
2025-11-01 01:40:00 +0530

    Add multiple contracts with ownership and secret management

--- 03.sol
+++ 03.sol
@@ -1,1 +1,41 @@
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract secondContract {
+    address owner;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "must be owner");
+        _;
+    }
+
+    constructor(string memory _secret) {
+        owner = msg.sender;
+    }
+}
+
+contract newContract is secondContract {
+    string private secret;
+
+    constructor(string memory _secret) secondContract(_secret) {
+        secret = _secret;
+    }
+
+    function getSecret() public view onlyOwner returns (string memory) {
+        return secret;
+    }
+}
+
+contract selfContract is secondContract {
+    address private secretvault;
+
+    constructor(string memory _secret) secondContract(_secret) {
+        newContract vault = new newContract(_secret);
+        secretvault = address(vault);
+    }
+
+    function showString() public view onlyOwner returns (string memory) {
+        return newContract(secretvault).getSecret();
+    }
+}
